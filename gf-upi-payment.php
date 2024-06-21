4eb97a273d9a9ad1a0901cf9ca20c8c3b8839154
sachindingli <sachindingli@gmail.com>
2024-06-21 12:04:42 +0400

    Update gf-upi-payment.php

--- gf-upi-payment.php
+++ gf-upi-payment.php
@@ -1,16 +1,24 @@
 <?php
 /*
 Plugin Name: Gravity Forms UPI Payment
-Plugin URI:  http://yourwebsite.com
+Plugin URI:  http://eworldservice.com
 Description: A plugin to integrate UPI payments with Gravity Forms.
 Version:     1.0
-Author:      Your Name
-Author URI:  http://yourwebsite.com
+Author:      sachin jangir
+Author URI:  http://eworldservice.com
 License:     GPL2
 */
 
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
+}
+
+// Ensure Gravity Forms is active
+if (!class_exists('GFForms')) {
+    add_action('admin_notices', function() {
+        echo '<div class="notice notice-error"><p>' . __('Gravity Forms must be installed and activated for the Gravity Forms UPI Payment plugin to work.', 'gf-upi-payment') . '</p></div>';
+    });
+    return;
 }
 
 // Include the main class
