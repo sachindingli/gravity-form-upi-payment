72f40e7eb239e17aad6e079c79fddab236ee448c
sachindingli <sachindingli@gmail.com>
2024-06-21 12:10:27 +0400

    Update admin-settings.php

--- templates/admin-settings.php
+++ templates/admin-settings.php
@@ -13,30 +13,3 @@ function gf_upi_payment_settings_page() {
     </div>
     <?php
 }
-
-function gf_upi_payment_register_settings() {
-    register_setting('gf_upi_payment_settings_group', 'gf_upi_payment_upi_id');
-    add_settings_section(
-        'gf_upi_payment_main_section',
-        __('Main Settings', 'gf-upi-payment'),
-        'gf_upi_payment_main_section_callback',
-        'gf-upi-payment'
-    );
-    add_settings_field(
-        'gf_upi_payment_upi_id',
-        __('Default UPI ID', 'gf-upi-payment'),
-        'gf_upi_payment_upi_id_callback',
-        'gf-upi-payment',
-        'gf_upi_payment_main_section'
-    );
-}
-add_action('admin_init', 'gf_upi_payment_register_settings');
-
-function gf_upi_payment_main_section_callback() {
-    echo '<p>' . __('Enter your default UPI ID for payments.', 'gf-upi-payment') . '</p>';
-}
-
-function gf_upi_payment_upi_id_callback() {
-    $upi_id = get_option('gf_upi_payment_upi_id');
-    echo '<input type="text" name="gf_upi_payment_upi_id" value="' . esc_attr($upi_id) . '" />';
-}
