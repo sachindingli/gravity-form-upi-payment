51324ea78fe2020fb189aea03d94be4072c47d07
sachindingli <sachindingli@gmail.com>
2024-06-21 13:04:27 +0400

    Update admin-settings.php

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -14,8 +14,27 @@ function gf_upi_payment_settings_page() {
     <?php
 }
 
+function gf_upi_payment_main_section_callback() {
+    echo '<p>' . __('Enter your default UPI ID and UPI Payment API URL.', 'gf-upi-payment') . '</p>';
+}
+
+function gf_upi_payment_upi_id_callback() {
+    $upi_id = get_option('gf_upi_payment_upi_id');
+    ?>
+    <input type="text" name="gf_upi_payment_upi_id" value="<?php echo esc_attr($upi_id); ?>" class="regular-text" />
+    <?php
+}
+
+function gf_upi_payment_api_url_callback() {
+    $api_url = get_option('gf_upi_payment_api_url');
+    ?>
+    <input type="text" name="gf_upi_payment_api_url" value="<?php echo esc_attr($api_url); ?>" class="regular-text" />
+    <?php
+}
+
 function gf_upi_payment_register_settings() {
     register_setting('gf_upi_payment_settings_group', 'gf_upi_payment_upi_id');
+    register_setting('gf_upi_payment_settings_group', 'gf_upi_payment_api_url');
     add_settings_section(
         'gf_upi_payment_main_section',
         __('Main Settings', 'gf-upi-payment'),
@@ -29,16 +48,11 @@ function gf_upi_payment_register_settings() {
         'gf-upi-payment',
         'gf_upi_payment_main_section'
     );
-}
-add_action('admin_init', 'gf_upi_payment_register_settings');
-
-function gf_upi_payment_main_section_callback() {
-    echo '<p>' . __('Enter your default UPI ID for payments.', 'gf-upi-payment') . '</p>';
-}
-
-function gf_upi_payment_upi_id_callback() {
-    $upi_id = get_option('gf_upi_payment_upi_id');
-    ?>
-    <input type="text" name="gf_upi_payment_upi_id" value="<?php echo esc_attr($upi_id); ?>" class="regular-text" />
-    <?php
+    add_settings_field(
+        'gf_upi_payment_api_url',
+        __('UPI Payment API URL', 'gf-upi-payment'),
+        'gf_upi_payment_api_url_callback',
+        'gf-upi-payment',
+        'gf_upi_payment_main_section'
+    );
 }
